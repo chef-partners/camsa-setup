3eacd673949ad954524416815a4af4a4d02e4000
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2019-09-12 09:05:33 +0100

    Corrected syntax error
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- cookbooks/camsa/recipes/kernel.rb
+++ cookbooks/camsa/recipes/kernel.rb
@@ -22,6 +22,6 @@ node['camsa']['automate']['kernel'].each do |name, value|
   sysctl name do
     value value
     
-    unless { node['camsa']['deploy']['supermarket'] }
+    not_if { node['camsa']['deploy']['supermarket'] }
   end
 end
